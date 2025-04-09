b82528d7835849c303b8f1f43c73d3ea49607ec8
AaronC714 <aaronc0714@gmail.com>
2025-04-09 20:40:58 +0800

    def main added to ask users input for questions choices and answeres

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,1 +1,9 @@
 #Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
+
+#user input of questions, choices, and answers
+def main():
+    filename = "questions.txt"
+    
+    while True:
+        print("\nEnter your multiple choice question:")
+        question = input("Question: ")
